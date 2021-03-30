ab08359f01c0440f6687226ceac1e2289c7e3988
cloxton2003 <cloxton2003@hotmail.com>
2021-03-30 13:35:17 -0600

    Added a scene with a couple buttons. Added a second scene as well that will be used as print tables

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnOptions;
+
+    @FXML
+    private Button btnTables;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    void initialize() {
+        assert btnOptions != null : "fx:id=\"btnOptions\" was not injected: check your FXML file 'sample.fxml'.";
+        assert btnTables != null : "fx:id=\"btnTables\" was not injected: check your FXML file 'sample.fxml'.";
+        assert btnExit != null : "fx:id=\"btnExit\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
 }
