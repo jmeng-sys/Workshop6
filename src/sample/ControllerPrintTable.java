665aca9c762475cc47b2d44230adb0c066f25d4d
cloxton2003 <cloxton2003@hotmail.com>
2021-03-31 10:39:52 -0600

    Got the table view populating with Data, the previous buttons and next buttons work and so does the exit button. combo box is loaded up. code just needs a clean.

--- src/sample/ControllerPrintTable.java
+++ src/sample/ControllerPrintTable.java
@@ -1,26 +1,38 @@
 package sample;
 
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Node;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.layout.StackPane;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-        import java.io.IOException;
-        import java.net.URL;
-        import java.util.ResourceBundle;
-        import javafx.fxml.FXML;
-        import javafx.scene.control.Button;
-        import javafx.stage.Stage;
+import database.DAO;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableStringValue;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class ControllerPrintTable {
-    private StackPane root;
-
+    private ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
+
+    @FXML
+    private ComboBox<String> cbTables;
 
     @FXML
     private Button btnOptions;
@@ -32,12 +44,34 @@ public class ControllerPrintTable {
     private Button btnExit;
 
     @FXML
+    private Button btnOptions1;
+
+    @FXML
+    private Button btnPrev;
+
+    @FXML
+    private Button btnNext;
+
+    @FXML
+    private TableView<ObservableList<String>> tableDatabase;
+
+    @FXML
     void initialize() {
+        assert cbTables != null : "fx:id=\"cbTables\" was not injected: check your FXML file 'PrintTable.fxml'.";
         assert btnOptions != null : "fx:id=\"btnOptions\" was not injected: check your FXML file 'PrintTable.fxml'.";
         assert btnTables != null : "fx:id=\"btnTables\" was not injected: check your FXML file 'PrintTable.fxml'.";
         assert btnExit != null : "fx:id=\"btnExit\" was not injected: check your FXML file 'PrintTable.fxml'.";
+        assert btnOptions1 != null : "fx:id=\"btnOptions1\" was not injected: check your FXML file 'PrintTable.fxml'.";
+        assert btnPrev != null : "fx:id=\"btnPrev\" was not injected: check your FXML file 'PrintTable.fxml'.";
+        assert btnNext != null : "fx:id=\"btnNext\" was not injected: check your FXML file 'PrintTable.fxml'.";
+        assert tableDatabase != null : "fx:id=\"tableDatabase\" was not injected: check your FXML file 'PrintTable.fxml'.";
 
 
+//  MENU BAR BUTTONS ===================================================================================================
+        btnExit.setOnMouseClicked(mouseEvent -> System.exit(0));
+        btnNext.setOnMouseClicked(mouseEvent -> cbTables.getSelectionModel().selectNext());
+        btnPrev.setOnMouseClicked(mouseEvent -> cbTables.getSelectionModel().selectPrevious());
+//  SET SCENE WHEN BUTTON CLICKED ======================================================================================
         btnTables.setOnMouseClicked(event -> {
             System.out.println("on route to sample");
             try {
@@ -49,7 +83,62 @@ public class ControllerPrintTable {
             }catch (IOException io){
                 io.printStackTrace();
             }
+        });
+//  SET COMBO BOX LIST AND LISTENERS ===================================================================================
+        try {
+            DatabaseMetaData databaseMetaData = DAO.getConnection().getMetaData();
+            ResultSet tables = databaseMetaData.getTables(
+                    "TRAVELEXPERTS",
+                    "1",
+                    null,
+                    new String[]{"TABLE"});
 
+            ObservableList<String> tableList = FXCollections.observableArrayList();
+
+            while (tables.next()) {
+                tableList.add(tables.getString("TABLE_NAME"));
+            }
+            cbTables.setItems(tableList);
+        } catch (SQLException e) {
+            System.out.println(e);
+        };
+//  BUILD TABLE AND COLUMNS ============================================================================================
+        cbTables.getSelectionModel().selectedItemProperty().addListener((observableValue, s, t1) -> {
+                tableDatabase.getItems().clear();
+                tableDatabase.getColumns().clear();
+            if (tableDatabase.getItems().isEmpty()) {
+                ObservableList<ObservableList<String>> items = tableDatabase.getItems();
+                if (items.isEmpty()) {
+
+                    data = FXCollections.observableArrayList();
+                    try {
+                        Connection conn = DAO.getConnection();
+                        String dataQuery = "SELECT * FROM " + t1;
+                        ResultSet tableValues = conn.createStatement().executeQuery(dataQuery);
+
+                        for (int i = 0; i < tableValues.getMetaData().getColumnCount(); i++) {
+                            final int j = i;
+                            TableColumn<ObservableList<String>, String> tableColumn = new TableColumn(tableValues.getMetaData().getColumnName(i + 1));
+                            tableColumn.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(j)));
+                            tableDatabase.getColumns().add(tableColumn);
+                        }
+
+                        while (tableValues.next()) {
+                            ObservableList<String> row = FXCollections.observableArrayList();
+                            for (int i = 1; i <= tableValues.getMetaData().getColumnCount(); i++) {
+                                row.add(tableValues.getString(i));
+                            }
+                            data.add(row);
+                        }
+                        tableDatabase.getItems().addAll(data);
+
+                        conn.close();
+
+                    } catch (SQLException e) {
+                        System.out.println(e);
+                    }
+                }
+            }
         });
     }
 }
