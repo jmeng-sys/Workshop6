24413647f4d4a0d4b8b39daf2bca779a55ebc304
Tristen Stockley <Tristen.Stockley@edu.sait.ca>
2021-04-04 14:33:07 -0600

    Created Login GUI. Updated style.css to included Login GUI. Button login onClick event. Created AgentAccountsDB. Deleted LoginHandler Class. Updated CreateAgentAccounts.sql

---
+++ src/database/AgentAccountsDB.java
@@ -0,0 +1,24 @@
+package database;
+
+public class AgentAccountsDB implements DAO
+{
+    private int agentId;
+    private String password;
+    private String username;
+
+    public AgentAccountsDB(int agentId, String password, String username)
+    {
+        this.agentId = agentId;
+        this.password = password;
+        this.username = username;
+    }
+
+    public int getAgentId() { return agentId; }
+    public void setAgentId(int agentId) { this.agentId = agentId; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+}
